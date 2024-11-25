a0c6b67ddbf822b700d27287ceaa78fbbd7f2559
Zak <djabazaki@gmail.com>
2024-11-25 18:01:16 +0100

    organised thinsg

---
+++ Zak's-try/Exo4.java
@@ -0,0 +1,19 @@
+
+// 4 - console application to deal with tables ( find max value/ find min value / order table AtoZ & ZtoA / deleting element from table / adding element to the table...)....
+// Alll based on the users input your console should decide which opération it should execute 
+import java.util.Scanner;
+
+public class Exo4 {
+    public static void main(String[] args) {
+
+        // Declaration of the input variable
+        Scanner input = new Scanner(System.in);
+
+        // Declaration of the table
+        int[]
+
+        public void fillTable(){
+
+        }
+    }
+}
