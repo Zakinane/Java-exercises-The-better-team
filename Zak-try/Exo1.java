e9e470e9922a6a2c4ce2d16a2bb98824450d45b6
Zak <djabazaki@gmail.com>
2024-11-26 23:58:11 +0100

    shoe rubbing for the stickers

--- Zak-try/Exo1.java
+++ Zak-try/Exo1.java
@@ -3,15 +3,27 @@
 import java.util.Scanner;
 
 public class Exo1 {
+
+    // for styleee
+    public static final String GREEN = "\u001B[32m";
+    public static final String RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         // Declaration of the input variable
         Scanner input = new Scanner(System.in);
-        
+
         // Getting the name
         System.out.print("Enter your name : ");
         String name = input.nextLine();
 
         // Printing the name
+        if (name.compareToIgnoreCase("Rayane") == 0) {
+            System.out.println("Hey " + GREEN + name + RESET + "!");
+            return;
+
+        }
+
         System.out.println("Hello " + name);
+
     }
 }
