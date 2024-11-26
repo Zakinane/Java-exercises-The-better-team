b4a3f264b1e8bbc7ff895abb8daa759a89ba5f7e
Zak <djabazaki@gmail.com>
2024-11-26 23:42:43 +0100

    exo4

--- Zak-try/Exo4.java
+++ Zak-try/Exo4.java
@@ -1,10 +1,12 @@
 // 4 - console application to deal with tables ( find max value/ find min value / order table AtoZ & ZtoA / deleting element from table / adding element to the table...)....
 // Alll based on the users input your console should decide which opération it should execute 
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exo4 {
+
+    // Just for styling purpose
     public static void cls() {
         System.out.println("\033[2J");
     }
@@ -14,93 +16,111 @@ public class Exo4 {
         // Declaration of the input variable
         Scanner input = new Scanner(System.in);
 
-        // User fills up the table
-        System.out.print("Enter the number of elements : ");
-        int tableSize = input.nextInt();
-
-        // Declaration + initialisation of the table
-        int[] table = new int[tableSize];
-
-        for (int i = 0; i < tableSize; i++) {
-            System.out.print("Element number " + (i + 1) + " : ");
-            table[i] = input.nextInt();
-        }
-        cls();
+        // Declaration of the table
+        ArrayList<Integer> table = new ArrayList<Integer>();
 
         int choice;
 
         do {
             System.out.println("[*] Choose an action to perform.");
             System.out.println("[0] Print the table.");
-            System.out.println("[1] Finding the smallest number on the list.");
-            System.out.println("[2] Finding the biggest number on the list.");
-            System.out.println("[3] Order from smallest to biggest.");
-            System.out.println("[4] Order from biggest to smallest.");
-            System.out.println("[5] Delete an element from the table.");
-            System.out.println("[6] Add an element to the table.");
+            System.out.println("[1] Add an element to the table.");
+            System.out.println("[2] Delete an element from the table.");
+            if (table.size() > 1) {
+                System.out.println("[3] Finding the smallest number on the list.");
+                System.out.println("[4] Finding the biggest number on the list.");
+                System.out.println("[5] Order from smallest to biggest.");
+                System.out.println("[6] Order from biggest to smallest.");
+            }
             System.out.println("[7] Exit the program.");
 
             choice = input.nextInt();
 
             switch (choice) {
+
                 case 0:
-                    System.out.println("\033[2J");
+                    cls();
                     System.out.print("[");
-                    for (int i = 0; i < table.length; i++)
-                        System.out.print(table[i] + ",");
-                    System.out.println("\b]");
+                    for (int i = 0; i < table.size(); i++)
+                        System.out.print(table.get(i) + ",");
+                    if (table.size() == 0)
+                        System.out.println("]\n");
+                    else
+                        System.out.println("\b]\n");
                     break;
+
                 case 1: // Finding the smallest number on the list
                     cls();
-                    int smallestNum = table[0];
-                    for (int i = 1; i < table.length; i++) {
-                        if (smallestNum > table[i])
-                            smallestNum = table[i];
-                    }
-                    System.out.println("The smallest number is : " + smallestNum +"\n");
-                    break;
-                case 2: // Finding the biggest number on the list
+                    System.out.print("Enter the number you want to add : ");
+                    int addedNum = input.nextInt();
+                    table.add(addedNum);
                     cls();
-                    int biggestNum = table[0];
-                    for (int i = 1; i < table.length; i++) {
-                        if (biggestNum < table[i])
-                            biggestNum = table[i];
-                    }
-                    System.out.println("The biggest number is : " + biggestNum + "\n");
+                    System.out.println(
+                            table.get(table.size() - 1) + " successfully added at index " + (table.size() - 1) + "\n");
                     break;
-                case 3: // Order from smallest to biggest
-                    cls();
-                    Arrays.sort(table);
-                    System.out.println("Table successfully sorted.\n");
-                    break;
-                case 4: // Order from biggest to smallest
-                    cls();
-                    for (int i = 0; i < table.length; i++) {
-                        table[i] = -table[i];
-                    }
-                    Arrays.sort(table);
-                    for (int i = 0; i < table.length; i++) {
-                        table[i] = -table[i];
-                    }
 
-                    System.out.println(Arrays.toString(table));
-                    System.out.println("Table successfully sorted.\n");
-                    break;
-                case 5: // Delete an element from the table
+                case 2: // Finding the biggest number on the list
                     cls();
                     System.out.print("Give the index of the element you want to remove : ");
                     int removedIndex = input.nextInt();
-                    table.remove(removedIndex);
+                    cls();
+                    if (removedIndex >= 0 && removedIndex < table.size()) {
+                        table.remove(removedIndex);
+                        System.out.println("Element removed successfully.\n");
+                    } else {
+                        System.out.println("[!] Invalid index.\n");
+                    }
                     break;
+
+                case 3: // Order from smallest to biggest
+                    cls();
+                    int smallestNum = table.get(0);
+                    for (int i = 1; i < table.size(); i++) {
+                        if (smallestNum > table.get(i))
+                            smallestNum = table.get(i);
+                    }
+                    System.out.println("The smallest number is : " + smallestNum + "\n");
+
+                    break;
+
+                case 4: // Order from biggest to smallest
+                    cls();
+                    int biggestNum = table.get(0);
+                    for (int i = 1; i < table.size(); i++) {
+                        if (biggestNum < table.get(i))
+                            biggestNum = table.get(i);
+                    }
+                    System.out.println("The biggest number is : " + biggestNum + "\n");
+                    break;
+
+                case 5: // Delete an element from the table
+                    cls();
+                    table.sort(null);
+                    System.out.println("Table successfully sorted.\n");
+
+                    break;
+
                 case 6: // Add an element to the table
                     cls();
+                    for (int i = 0; i < table.size(); i++) {
+                        table.set(i, -table.get(i));
+                    }
+                    table.sort(null);
+                    for (int i = 0; i < table.size(); i++) {
+                        table.set(i, -table.get(i));
+                    }
+
+                    System.out.println("Table successfully sorted.\n");
                     break;
+
                 case 7: // Exit the program
                     cls();
                     System.out.print("Goodbye!");
                     break;
+
                 default:
-                    System.out.println("Choose an existing action.");
+                    cls();
+                    System.out.println("[!] Choose an existing action.\n");
             }
         } while (choice != 7);
     }
