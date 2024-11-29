984c5961b9a33a8afff9e77022b3708dd49f56e2
Djasser <Djasseromeiri12@gmail.com>
2024-11-29 17:25:43 +0100

    exo7

--- Djasser-try/exo7.java
+++ Djasser-try/exo7.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+import java.util.Random;
+
 public class exo7 {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        
-    }
+        int[] list = {6, 0, 0, 7, 18, 18, 1, 5};
+        Random rand = new Random();
 
+        int randomIndex = rand.nextInt(list.length);
+        System.out.println("Random number picked: " + list[randomIndex]);
+    }
 }
